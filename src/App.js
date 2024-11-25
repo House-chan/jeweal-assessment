167448bdabb90bdc230de78783f7a8ffcb5b4e22
House-chan <house3d@hotmail.com>
2024-11-25 15:19:51 +0700

    Implement Product List component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,48 @@
 import logo from './logo.svg';
 import ReceiptDetail from './components/ReceiptDetail';
-
+import ProductList from './components/ProductList';
+import SummaryDetail from './components/SummaryDetail';
+import { useState } from 'react';
 function App() {
-  return (
-    <div className="App">
-      <ReceiptDetail/>
-    </div>
-  );
+
+	const [productRow, setProductRow] = useState([
+		{ id: "", quantity: "", weight: "", price: "", discountCode: "", discount: 0, total:""},
+		{ id: "", quantity: "", weight: "", price: "", discountCode: "", discount: 0, total:""},
+		{ id: "", quantity: "", weight: "", price: "", discountCode: "", discount: 0, total:""},
+		{ id: "", quantity: "", weight: "", price: "", discountCode: "", discount: 0, total:""},
+	])
+
+	const totalPrice = productRow.reduce((acc, product) => acc + (parseFloat(product.total) || 0), 0);
+	const totalPriceBeforeDiscount = productRow.reduce(
+	(acc, product) => acc + ((parseFloat(product.price) || 0) * (parseFloat(product.quantity) || 0)),
+	0
+	);
+	const totalDiscount = totalPriceBeforeDiscount - totalPrice
+	const totalDetail = {
+		quantity: productRow.reduce((acc, product) => acc + (parseFloat(product.weight) || 0), 0),
+		weight:productRow.reduce((acc, product) => acc + (parseFloat(product.weight) || 0), 0),
+		price: totalPrice,
+          priceWoDiscount: totalPriceBeforeDiscount,
+          totalDiscount: totalDiscount
+	}
+
+	return (
+		<div className="mx-5 mt-5">
+			<ReceiptDetail/>
+			<div className="w-full flex">
+				<div className="w-[60vw]">
+					<ProductList 
+						productRow={productRow}
+						setProductRow={setProductRow}
+						totalDetail={totalDetail}
+					/>
+				</div>
+				<div className="w-[40vw]">
+					<SummaryDetail/>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/ProductList.jsx
@@ -0,0 +1,214 @@
+import React from 'react'
+import { useState } from 'react'
+
+const ProductList = ({currency, productRow, setProductRow, totalDetail}) => {
+
+	const productData = {
+		"P001": { weight: 1.2, price: 10 },
+		"P002": { weight: 0.5, price: 5 },
+		"P003": { weight: 2.0, price: 20 },
+		"P004": { weight: 0.8, price: 8 },
+	};
+
+	//? use in data list
+	const productList = [
+		"P001",
+		"P002",
+		"P003",
+		"P004",
+	];
+	
+
+
+	const discountCode = {
+		"DISCOUNT10": 10,
+          "DISCOUNT20": 20,
+          "DISCOUNT30": 30,
+	}
+
+	const replaceProduct = (index, product) =>{
+		let updatedProduct = [...productRow];
+		updatedProduct[index] = product;
+          setProductRow(updatedProduct);
+	}
+
+	const handleProductId = (index, value) =>{
+		let updatedProduct = productRow[index];
+		updatedProduct["id"] = value;
+		
+		//? Check product exist
+		const product = productData[value];
+
+		if (product) {
+			updatedProduct.price = product.price;
+			updatedProduct.weight = product.weight;
+			updatedProduct.total = updatedProduct.quantity * updatedProduct.price * (1 - (updatedProduct.discountCode / 100));
+			updatedProduct.quantity = 1;
+		}
+		else{
+			updatedProduct.price = "";
+			updatedProduct.weight = "";
+			updatedProduct.total = "";
+			updatedProduct.quantity = "";
+		}
+		replaceProduct(index, updatedProduct);
+	}
+
+	const handleProductDiscount = (index, value) =>{
+		let updatedProduct = productRow[index];
+		updatedProduct["discountCode"] = value;
+		// discount Valid
+		const discount = discountCode[value];
+		
+		if (discount) {
+			updatedProduct.discount = discount;
+			updatedProduct.total = (updatedProduct.quantity * updatedProduct.price) * (1 - (updatedProduct.discount / 100));
+		}
+		replaceProduct(index, updatedProduct);
+	}
+
+	const handleProductQuantity = (index, value) =>{
+          let updatedProduct = productRow[index];
+          updatedProduct["quantity"] = value;
+          updatedProduct.total = updatedProduct.quantity * updatedProduct.price * (1 - (updatedProduct.discount / 100));
+		replaceProduct(index, updatedProduct);
+     }
+
+	const addrow = () =>{
+		let newRow = [...productRow];
+          newRow.push({ id: "", quantity: "", weight: "", price: "", discountCode: "", total:""});
+          setProductRow(newRow);
+	}
+
+	const deleterow = (index) =>{
+		let newRow = [...productRow];
+          newRow.splice(index, 1);
+		setProductRow(newRow);
+	}
+
+	return (
+		<div className="w-full py-5 px-10 bg-gray-100">
+			<div className="text-green-700 font-bold text-xl mb-5">
+				Product List
+			</div>
+			{/* Product List */}
+			<div className="w-auto bg-white h-[300px] max-h-[300px] overflow-y-scroll mx-10 mb-5">
+				<table className="w-full table-auto">
+					<thead className="sticky">
+						<tr className="bg-gray-200">
+							<th className="table-header">#</th>
+							<th className="table-header">Id</th>
+							<th className="table-header">Quantity</th>
+							<th className="table-header border-x-2 border-gray-300">Weight</th>
+							<th className="table-header">Price/unit</th>
+							<th className="table-header">Unit</th>
+							<th className="table-header">Prices <br /> w/o Discount</th>
+							<th className="table-header border-x-2 border-gray-300">Discount Code</th>
+							<th className="table-header">Total</th>
+						</tr>
+					</thead>
+					<tbody>
+						{productRow.map((product, index) => (
+							<tr key={index} className="w-full">
+								<td className="table-body text-center">{index + 1}</td>
+                                        <td className="table-body w-32">
+                                             <input 
+										list="ProductId" 
+                                                  value={product.id} 
+                                                  onChange={(e) => handleProductId(index, e.target.value)} 
+										placeholder=""
+                                                  className="table-body-input" 
+                                             />
+									<datalist id="ProductId">
+										{productList.map((option, index) => (
+											<option key={index} value={option} />
+										))}
+									</datalist>
+                                        </td>
+								<td className="table-body w-16">
+									<input 
+                                                  type="number" 
+                                                  value={product.quantity} 
+                                                  onChange={(e) => handleProductQuantity(index, e.target.value)} 
+                                                  placeholder=""
+                                                  className="table-body-input" 
+                                             />
+								</td>
+								<td className="table-body border-x border-gray-300">
+									<p className="table-body-text">
+										{product.weight}
+									</p>
+								</td>
+              						<td className="table-body">
+								    	<p className="table-body-text">
+										{product.price}
+									</p>
+								</td>
+								<td className="table-body">
+									<p className="table-body-text">
+										THB
+									</p>
+								</td>
+								<td className="table-body">
+									<p className="table-body-text">
+										{product.price * product.quantity !== 0 ? product.price * product.quantity : ""}
+									</p>
+								</td>
+								<td className="table-body w-32 border-x-2 border-gray-300">
+									<input 
+                                                  type="text" 
+                                                  value={product.discountCode} 
+                                                  onChange={(e) => handleProductDiscount(index, e.target.value)} 
+                                                  placeholder=""
+                                                  className="table-body-input" 
+                                             />
+								</td>
+								<td className="table-body">
+									<p className="table-body-text">
+										{product.total}
+									</p>
+                                        </td>
+								<td className="table-body w-10 text-sm">
+									<button onClick={() => deleterow(index)} className="text-blue-600">Delete <br/> Row</button>
+                                		</td>
+							</tr>
+						))}
+						<tr className="border-t-2 border-gray-300">
+							<th>
+								
+							</th>
+							<td>
+
+							</td>
+							<th className="table-body-summary">
+								Quantity
+							</th>
+							<th className="table-body-summary border-x-2 border-gray-300">
+								Weight
+							</th>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td className="border-x-2 border-gray-300"></td>
+							<th className="table-body-summary">
+								Total Price
+							</th>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+			<div className="flex justify-start mb-5">
+				<button onClick={addrow} className="border-2 px-2 py-1 bg-gray-200 rounded-lg text-black">Add Row</button>
+			</div>
+
+			<textarea 
+				type="text"
+				placeholder='Remark'
+				className="border-2 w-full rounded-lg h-32 p-2 align-top resize-none"
+			/>
+
+		</div>
+	)
+}
+
+export default ProductList
